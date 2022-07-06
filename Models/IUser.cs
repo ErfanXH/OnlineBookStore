07a00342a8f3aaf4c6cc8a45b752f5d5cd2bdcc2
Mohammad <Mohammadshamekhi4@gmail.com>
2022-07-06 10:25:40 +0430

    add Models

---
+++ Models/IUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public interface IUser
+    {
+        string FirtName { get; set; }
+        string LastName { get; set; }
+        string PhoneNumber { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        double AccountBalance { get; set; }
+        bool VIP { get; set; }
+        ObservableCollection<Book> BasketOfGoods { get; set; }
+        ObservableCollection<Book> Books { get; set; }
+        ObservableCollection<Book> BookMark { get; set; }
+    }
+}
